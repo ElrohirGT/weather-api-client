8b4785468ad037f2368ed541387e1853389837ef
ElrohirGT <elrohirgt@gmail.com>
2022-04-28 18:55:26 -0600

    Cleaned the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//This should be hidden. Maybe using an external service, but this is for practice purposes.
+const API_KEY = "e26a200fe8cd4b9995702100222904";
+const FORECAST_ENDPOINT = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&aqi=no&alerts=no`;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App forecast={FORECAST_ENDPOINT}/>
   </React.StrictMode>
 );
 
